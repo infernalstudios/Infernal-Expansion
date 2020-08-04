d7442ad4ae0327b0d03c97fd7ab1a51c778285cc
BaconKami <natou022@gmail.com>
2020-08-04 15:50:10 -0400

    Overall code refactoring, added glowdust layers, removed glowsquito bricks

--- src/main/java/com/nekomaster1000/Infernal/init/ModBlocks.java
+++ src/main/java/com/nekomaster1000/Infernal/init/ModBlocks.java
@@ -2,12 +2,10 @@ package com.nekomaster1000.Infernal.init;
 
 import com.nekomaster1000.Infernal.InfernalExpansion;
 import com.nekomaster1000.Infernal.blocks.*;
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.SandBlock;
-import net.minecraft.block.SoundType;
+import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -17,24 +15,30 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, InfernalExpansion.MOD_ID);
 
     // Blocks
-    public static final RegistryObject<Block> DIMSTONE = BLOCKS.register("dimstone", Dimstone::new);
-    public static final RegistryObject<Block> DULLSTONE = BLOCKS.register("dullstone", Dullstone::new);
-    public static final RegistryObject<Block> GLOWSTONE_SMOOTH = BLOCKS.register("glowstone_smooth", GlowstoneSmooth::new);
-    public static final RegistryObject<Block> DIMSTONE_SMOOTH = BLOCKS.register("dimstone_smooth", DimstoneSmooth::new);
-    public static final RegistryObject<Block> DULLSTONE_SMOOTH = BLOCKS.register("dullstone_smooth", DullstoneSmooth::new);
-    public static final RegistryObject<Block> GLOWSTONE_BRICK = BLOCKS.register("glowstone_brick", GlowstoneBrick::new);
-    public static final RegistryObject<Block> DIMSTONE_BRICK = BLOCKS.register("dimstone_brick", DimstoneBrick::new);
-    public static final RegistryObject<Block> DULLSTONE_BRICK = BLOCKS.register("dullstone_brick", DullstoneBrick::new);
-    public static final RegistryObject<Block> GLOWSQUITO_GLOWSTONE_BRICK = BLOCKS.register("glowsquito_glowstone_brick", GlowsquitoGlowstoneBrick::new);
-    public static final RegistryObject<Block> CHISELED_GLOWSTONE_BRICK = BLOCKS.register("chiseled_glowstone_brick", ChiselledGlowstoneBrick::new);
-    public static final RegistryObject<Block> GLOWSQUITO_DIMSTONE_BRICK = BLOCKS.register("glowsquito_dimstone_brick", GlowsquitoDimstoneBrick::new);
-    public static final RegistryObject<Block> CHISELED_DIMSTONE_BRICK = BLOCKS.register("chiseled_dimstone_brick", ChiselledDimstoneBrick::new);
-    public static final RegistryObject<Block> GLOWSQUITO_DULLSTONE_BRICK = BLOCKS.register("glowsquito_dullstone_brick", GlowsquitoDullstoneBrick::new);
-    public static final RegistryObject<Block> CHISELED_DULLSTONE_BRICK = BLOCKS.register("chiseled_dullstone_brick", ChiselledDullstoneBrick::new);
-    public static final RegistryObject<Block> GLOWDUST_SAND = BLOCKS.register("glowdust_sand", () -> new SandBlock(0xFFC267, AbstractBlock.Properties.create(Material.SAND).hardnessAndResistance(0.5F).sound(SoundType.SAND)));
-    public static final RegistryObject<Block> GLOWDUST_SANDSTONE = BLOCKS.register("glowdust_sandstone", () -> new Block(AbstractBlock.Properties.create(Material.ROCK).setRequiresTool().hardnessAndResistance(0.8F)));
-    public static final RegistryObject<Block> CUT_GLOWDUST_SANDSTONE = BLOCKS.register("cut_glowdust_sandstone", () -> new Block(AbstractBlock.Properties.create(Material.ROCK).setRequiresTool().hardnessAndResistance(0.8F)));
-    public static final RegistryObject<Block> CHISELED_GLOWDUST_SANDSTONE = BLOCKS.register("chiseled_glowdust_sandstone", () -> new Block(AbstractBlock.Properties.create(Material.ROCK).setRequiresTool().hardnessAndResistance(0.8F)));
-    public static final RegistryObject<Block> SMOOTH_GLOWDUST_SANDSTONE = BLOCKS.register("smooth_glowdust_sandstone", () -> new Block(AbstractBlock.Properties.create(Material.ROCK).setRequiresTool().hardnessAndResistance(2.0F, 6.0F)));
+    public static final RegistryObject<Block> DIMSTONE = BLOCKS.register("dimstone", () -> new Block(getBasicProperties(Material.GLASS, 4.0F, 5.0F).sound(SoundType.GLASS).setRequiresTool().setLightLevel(value -> 10)));
+    public static final RegistryObject<Block> DULLSTONE = BLOCKS.register("dullstone", () -> new Block(getBasicProperties(Material.GLASS, 4.0F, 5.0F).sound(SoundType.GLASS).setRequiresTool()));
+    public static final RegistryObject<Block> SMOOTH_GLOWSTONE = BLOCKS.register("smooth_glowstone", () -> new Block(getBasicProperties(Material.GLASS, 4.0F, 5.0F).sound(SoundType.GLASS).setRequiresTool().setLightLevel(value -> 15)));
+    public static final RegistryObject<Block> SMOOTH_DIMSTONE = BLOCKS.register("smooth_dimstone", () -> new Block(getBasicProperties(Material.GLASS, 4.0F, 5.0F).sound(SoundType.GLASS).setRequiresTool().setLightLevel(value -> 10)));
+    public static final RegistryObject<Block> SMOOTH_DULLSTONE = BLOCKS.register("smooth_dullstone", () -> new Block(getBasicProperties(Material.GLASS, 4.0F, 5.0F).sound(SoundType.GLASS).setRequiresTool()));
+    public static final RegistryObject<Block> GLOWSTONE_BRICK = BLOCKS.register("glowstone_brick", () -> new Block(getBasicProperties(Material.GLASS, 4.0F, 5.0F).sound(SoundType.GLASS).setRequiresTool().setLightLevel(value -> 15)));
+    public static final RegistryObject<Block> DIMSTONE_BRICK = BLOCKS.register("dimstone_brick", () -> new Block(getBasicProperties(Material.GLASS, 4.0F, 5.0F).sound(SoundType.GLASS).setRequiresTool().setLightLevel(value -> 10)));
+    public static final RegistryObject<Block> DULLSTONE_BRICK = BLOCKS.register("dullstone_brick", () -> new Block(getBasicProperties(Material.GLASS, 4.0F, 5.0F).sound(SoundType.GLASS).setRequiresTool()));
+    public static final RegistryObject<Block> CHISELED_GLOWSTONE_BRICK = BLOCKS.register("chiseled_glowstone_brick", () -> new Block(getBasicProperties(Material.GLASS, 4.0F, 5.0F).sound(SoundType.GLASS).setRequiresTool().setLightLevel(value -> 15)));
+    public static final RegistryObject<Block> CHISELED_DIMSTONE_BRICK = BLOCKS.register("chiseled_dimstone_brick", () -> new Block(getBasicProperties(Material.GLASS, 4.0F, 5.0F).sound(SoundType.GLASS).setRequiresTool().setLightLevel(value -> 10)));
+    public static final RegistryObject<Block> CHISELED_DULLSTONE_BRICK = BLOCKS.register("chiseled_dullstone_brick", () -> new Block(getBasicProperties(Material.GLASS, 4.0F, 5.0F).sound(SoundType.GLASS).setRequiresTool()));
+    public static final RegistryObject<Block> GLOWDUST_SAND = BLOCKS.register("glowdust_sand", () -> new SandBlock(0xFFC267, getBasicProperties(Material.SAND, 0.5F).sound(SoundType.SAND)));
+    public static final RegistryObject<Block> GLOWDUST_SANDSTONE = BLOCKS.register("glowdust_sandstone", () -> new Block(getBasicProperties(Material.ROCK, 0.8F).setRequiresTool()));
+    public static final RegistryObject<Block> CUT_GLOWDUST_SANDSTONE = BLOCKS.register("cut_glowdust_sandstone", () -> new Block(getBasicProperties(Material.ROCK, 0.8F).setRequiresTool()));
+    public static final RegistryObject<Block> CHISELED_GLOWDUST_SANDSTONE = BLOCKS.register("chiseled_glowdust_sandstone", () -> new Block(getBasicProperties(Material.ROCK, 0.8F).setRequiresTool()));
+    public static final RegistryObject<Block> SMOOTH_GLOWDUST_SANDSTONE = BLOCKS.register("smooth_glowdust_sandstone", () -> new Block(getBasicProperties(Material.ROCK, 2.0F, 6.0F).setRequiresTool()));
+    public static final RegistryObject<Block> GLOWDUST = BLOCKS.register("glowdust", () ->  new SnowBlock(getBasicProperties(Material.SAND, 0.1F).tickRandomly().setRequiresTool().sound(SoundType.SAND)));
 
+
+    public static AbstractBlock.Properties getBasicProperties(Material materialIn, float hardnessAndResistanceIn) {
+        return getBasicProperties(materialIn, hardnessAndResistanceIn, hardnessAndResistanceIn);
+    }
+
+    public static AbstractBlock.Properties getBasicProperties(Material materialIn, float hardnessIn, float resistanceIn) {
+        return AbstractBlock.Properties.create(materialIn).hardnessAndResistance(hardnessIn, resistanceIn);
+    }
 }
--- src/main/java/com/nekomaster1000/Infernal/init/ModItems.java
+++ src/main/java/com/nekomaster1000/Infernal/init/ModItems.java
@@ -18,90 +18,36 @@ public class ModItems {
     public static final RegistryObject<Item> GLOWCOAL = ITEMS.register("glowcoal", ItemBase::new);
     public static final RegistryObject<Item> DIMROCKS = ITEMS.register("glownuggets", ItemBase::new);
 
-    public static final RegistryObject<ModSpawnEggItem> GLOWSQUITO_SPAWN_EGG = ITEMS.register("glowsquito_spawn_egg",
-            () -> new ModSpawnEggItem(ModEntityType.GLOWSQUITO, 0xF0A5A2, 0xA9672B, new Item.Properties().group(InfernalExpansion.TAB)));
-
-    public static final RegistryObject<ModSpawnEggItem> VOLINE_SPAWN_EGG = ITEMS.register("voline_spawn_egg",
-            () -> new ModSpawnEggItem(ModEntityType.VOLINE, 0xF0A5A2, 0xA9672B, new Item.Properties().group(InfernalExpansion.TAB)));
-
-    public static final RegistryObject<ModSpawnEggItem> PYRNO_SPAWN_EGG = ITEMS.register("pyrno_spawn_egg",
-            () -> new ModSpawnEggItem(ModEntityType.PYRNO, 0xF0A5A2, 0xA9672B, new Item.Properties().group(InfernalExpansion.TAB)));
-
-    public static final RegistryObject<ModSpawnEggItem> WARPBEETLE_SPAWN_EGG = ITEMS.register("warpbeetle_spawn_egg",
-            () -> new ModSpawnEggItem(ModEntityType.WARPBEETLE, 0xF0A5A2, 0xA9672B, new Item.Properties().group(InfernalExpansion.TAB)));
+    // Spawn Eggs
+    public static final RegistryObject<ModSpawnEggItem> GLOWSQUITO_SPAWN_EGG = ITEMS.register("glowsquito_spawn_egg", () -> new ModSpawnEggItem(ModEntityType.GLOWSQUITO, 0xF0A5A2, 0xA9672B, new Item.Properties().group(InfernalExpansion.TAB)));
+    public static final RegistryObject<ModSpawnEggItem> VOLINE_SPAWN_EGG = ITEMS.register("voline_spawn_egg", () -> new ModSpawnEggItem(ModEntityType.VOLINE, 0xF0A5A2, 0xA9672B, new Item.Properties().group(InfernalExpansion.TAB)));
+    public static final RegistryObject<ModSpawnEggItem> PYRNO_SPAWN_EGG = ITEMS.register("pyrno_spawn_egg", () -> new ModSpawnEggItem(ModEntityType.PYRNO, 0xF0A5A2, 0xA9672B, new Item.Properties().group(InfernalExpansion.TAB)));
+    public static final RegistryObject<ModSpawnEggItem> WARPBEETLE_SPAWN_EGG = ITEMS.register("warpbeetle_spawn_egg", () -> new ModSpawnEggItem(ModEntityType.WARPBEETLE, 0xF0A5A2, 0xA9672B, new Item.Properties().group(InfernalExpansion.TAB)));
 
     // Block items
-    public static final RegistryObject<Item> DIMSTONE_ITEM = ITEMS.register("dimstone",
-            () -> new BlockItemBase(ModBlocks.DIMSTONE.get()));
-
-    public static final RegistryObject<Item> DULLSTONE_ITEM = ITEMS.register("dullstone",
-            () -> new BlockItemBase(ModBlocks.DULLSTONE.get()));
-
-    public static final RegistryObject<Item> GLOWSTONE_BRICK = ITEMS.register("glowstone_brick",
-            () -> new BlockItemBase(ModBlocks.GLOWSTONE_BRICK.get()));
-
-    public static final RegistryObject<Item> DIMSTONE_BRICK = ITEMS.register("dimstone_brick",
-            () -> new BlockItemBase(ModBlocks.DIMSTONE_BRICK.get()));
-
-    public static final RegistryObject<Item> DULLSTONE_BRICK = ITEMS.register("dullstone_brick",
-            () -> new BlockItemBase(ModBlocks.DULLSTONE_BRICK.get()));
-
-    public static final RegistryObject<Item> GLOWSTONE_SMOOTH = ITEMS.register("glowstone_smooth",
-            () -> new BlockItemBase(ModBlocks.GLOWSTONE_SMOOTH.get()));
-
-    public static final RegistryObject<Item> DIMSTONE_SMOOTH = ITEMS.register("dimstone_smooth",
-            () -> new BlockItemBase(ModBlocks.DIMSTONE_SMOOTH.get()));
-
-    public static final RegistryObject<Item> DILLSTONE_SMOOTH = ITEMS.register("dullstone_smooth",
-            () -> new BlockItemBase(ModBlocks.DULLSTONE_SMOOTH.get()));
-
-    public static final RegistryObject<Item> GLOWSQUITO_GLOWSTONE_BRICK = ITEMS.register("glowsquito_glowstone_brick",
-            () -> new BlockItemBase(ModBlocks.GLOWSQUITO_GLOWSTONE_BRICK.get()));
-
-    public static final RegistryObject<Item> CHISELED_GLOWSTONE_BRICK = ITEMS.register("chiseled_glowstone_brick",
-            () -> new BlockItemBase(ModBlocks.CHISELED_GLOWSTONE_BRICK.get()));
-
-    public static final RegistryObject<Item> GLOWSQUITO_DIMSTONE_BRICK = ITEMS.register("glowsquito_dimstone_brick",
-            () -> new BlockItemBase(ModBlocks.GLOWSQUITO_DIMSTONE_BRICK.get()));
-
-    public static final RegistryObject<Item> CHISELED_DIMSTONE_BRICK = ITEMS.register("chiseled_dimstone_brick",
-            () -> new BlockItemBase(ModBlocks.CHISELED_DIMSTONE_BRICK.get()));
-
-    public static final RegistryObject<Item> GLOWSQUITO_DULLSTONE_BRICK = ITEMS.register("glowsquito_dullstone_brick",
-            () -> new BlockItemBase(ModBlocks.GLOWSQUITO_DULLSTONE_BRICK.get()));
-
-    public static final RegistryObject<Item> CHISELED_DULLSTONE_BRICK = ITEMS.register("chiseled_dullstone_brick",
-            () -> new BlockItemBase(ModBlocks.CHISELED_DULLSTONE_BRICK.get()));
-
-    public static final RegistryObject<Item> GLOWDUST_SAND = ITEMS.register("glowdust_sand",
-            () -> new BlockItemBase(ModBlocks.GLOWDUST_SAND.get()));
-
-    public static final RegistryObject<Item> GLOWDUST_SANDSTONE = ITEMS.register("glowdust_sandstone",
-            () -> new BlockItemBase(ModBlocks.GLOWDUST_SANDSTONE.get()));
-
-    public static final RegistryObject<Item> CUT_GLOWDUST_SANDSTONE = ITEMS.register("cut_glowdust_sandstone",
-            () -> new BlockItemBase(ModBlocks.CUT_GLOWDUST_SANDSTONE.get()));
-
-    public static final RegistryObject<Item> CHISELED_GLOWDUST_SANDSTONE = ITEMS.register("chiseled_glowdust_sandstone",
-            () -> new BlockItemBase(ModBlocks.CHISELED_GLOWDUST_SANDSTONE.get()));
-
-    public static final RegistryObject<Item> SMOOTH_GLOWDUST_SANDSTONE = ITEMS.register("smooth_glowdust_sandstone",
-            () -> new BlockItemBase(ModBlocks.SMOOTH_GLOWDUST_SANDSTONE.get()));
+    public static final RegistryObject<Item> DIMSTONE = ITEMS.register("dimstone", () -> new BlockItemBase(ModBlocks.DIMSTONE.get()));
+    public static final RegistryObject<Item> DULLSTONE = ITEMS.register("dullstone", () -> new BlockItemBase(ModBlocks.DULLSTONE.get()));
+    public static final RegistryObject<Item> SMOOTH_GLOWSTONE = ITEMS.register("smooth_glowstone", () -> new BlockItemBase(ModBlocks.SMOOTH_GLOWSTONE.get()));
+    public static final RegistryObject<Item> SMOOTH_DIMSTONE = ITEMS.register("smooth_dimstone", () -> new BlockItemBase(ModBlocks.SMOOTH_DIMSTONE.get()));
+    public static final RegistryObject<Item> SMOOTH_DULLSTONE_ = ITEMS.register("smooth_dullstone", () -> new BlockItemBase(ModBlocks.SMOOTH_DULLSTONE.get()));
+    public static final RegistryObject<Item> GLOWSTONE_BRICK = ITEMS.register("glowstone_brick", () -> new BlockItemBase(ModBlocks.GLOWSTONE_BRICK.get()));
+    public static final RegistryObject<Item> DIMSTONE_BRICK = ITEMS.register("dimstone_brick", () -> new BlockItemBase(ModBlocks.DIMSTONE_BRICK.get()));
+    public static final RegistryObject<Item> DULLSTONE_BRICK = ITEMS.register("dullstone_brick", () -> new BlockItemBase(ModBlocks.DULLSTONE_BRICK.get()));
+    public static final RegistryObject<Item> CHISELED_GLOWSTONE_BRICK = ITEMS.register("chiseled_glowstone_brick", () -> new BlockItemBase(ModBlocks.CHISELED_GLOWSTONE_BRICK.get()));
+    public static final RegistryObject<Item> CHISELED_DIMSTONE_BRICK = ITEMS.register("chiseled_dimstone_brick", () -> new BlockItemBase(ModBlocks.CHISELED_DIMSTONE_BRICK.get()));
+    public static final RegistryObject<Item> CHISELED_DULLSTONE_BRICK = ITEMS.register("chiseled_dullstone_brick", () -> new BlockItemBase(ModBlocks.CHISELED_DULLSTONE_BRICK.get()));
+    public static final RegistryObject<Item> GLOWDUST_SAND = ITEMS.register("glowdust_sand", () -> new BlockItemBase(ModBlocks.GLOWDUST_SAND.get()));
+    public static final RegistryObject<Item> GLOWDUST_SANDSTONE = ITEMS.register("glowdust_sandstone", () -> new BlockItemBase(ModBlocks.GLOWDUST_SANDSTONE.get()));
+    public static final RegistryObject<Item> CUT_GLOWDUST_SANDSTONE = ITEMS.register("cut_glowdust_sandstone", () -> new BlockItemBase(ModBlocks.CUT_GLOWDUST_SANDSTONE.get()));
+    public static final RegistryObject<Item> CHISELED_GLOWDUST_SANDSTONE = ITEMS.register("chiseled_glowdust_sandstone", () -> new BlockItemBase(ModBlocks.CHISELED_GLOWDUST_SANDSTONE.get()));
+    public static final RegistryObject<Item> SMOOTH_GLOWDUST_SANDSTONE = ITEMS.register("smooth_glowdust_sandstone", () -> new BlockItemBase(ModBlocks.SMOOTH_GLOWDUST_SANDSTONE.get()));
+    public static final RegistryObject<Item> GLOWDUST = ITEMS.register("glowdust", () -> new BlockItemBase(ModBlocks.GLOWDUST.get()));
 
 
     // Tools
-    public static final RegistryObject<SwordItem> FROSTBITTEN_SWORD = ITEMS.register("frostbitten_sword",
-            () -> new SwordItem(ModItemTier.FROSTBITTEN_NETHERITE, 2, -2.4F, new Item.Properties().group(InfernalExpansion.TAB)));
-
-    public static final RegistryObject<PickaxeItem> FROSTBITTEN_PICKAXE = ITEMS.register("frostbitten_pickaxe",
-            () -> new PickaxeItem(ModItemTier.FROSTBITTEN_NETHERITE, 1, -2.4F, new Item.Properties().group(InfernalExpansion.TAB)));
-
-    public static final RegistryObject<AxeItem> FROSTBITTEN_AXE = ITEMS.register("frostbitten_axe",
-            () -> new AxeItem(ModItemTier.FROSTBITTEN_NETHERITE, 4, -3.1F, new Item.Properties().group(InfernalExpansion.TAB)));
-
-    public static final RegistryObject<ShovelItem> FROSTBITTEN_SHOVEL = ITEMS.register("frostbitten_shovel",
-            () -> new ShovelItem(ModItemTier.FROSTBITTEN_NETHERITE, -1, -2.4F, new Item.Properties().group(InfernalExpansion.TAB)));
-
-    public static final RegistryObject<HoeItem> FROSTBITTEN_HOE = ITEMS.register("frostbitten_hoe",
-            () -> new HoeItem(ModItemTier.FROSTBITTEN_NETHERITE, -2, -1.0F, new Item.Properties().group(InfernalExpansion.TAB)));
+    public static final RegistryObject<SwordItem> FROSTBITTEN_SWORD = ITEMS.register("frostbitten_sword", () -> new SwordItem(ModItemTier.FROSTBITTEN_NETHERITE, 2, -2.4F, new Item.Properties().group(InfernalExpansion.TAB)));
+    public static final RegistryObject<PickaxeItem> FROSTBITTEN_PICKAXE = ITEMS.register("frostbitten_pickaxe", () -> new PickaxeItem(ModItemTier.FROSTBITTEN_NETHERITE, 1, -2.4F, new Item.Properties().group(InfernalExpansion.TAB)));
+    public static final RegistryObject<AxeItem> FROSTBITTEN_AXE = ITEMS.register("frostbitten_axe", () -> new AxeItem(ModItemTier.FROSTBITTEN_NETHERITE, 4, -3.1F, new Item.Properties().group(InfernalExpansion.TAB)));
+    public static final RegistryObject<ShovelItem> FROSTBITTEN_SHOVEL = ITEMS.register("frostbitten_shovel", () -> new ShovelItem(ModItemTier.FROSTBITTEN_NETHERITE, -1, -2.4F, new Item.Properties().group(InfernalExpansion.TAB)));
+    public static final RegistryObject<HoeItem> FROSTBITTEN_HOE = ITEMS.register("frostbitten_hoe", () -> new HoeItem(ModItemTier.FROSTBITTEN_NETHERITE, -2, -1.0F, new Item.Properties().group(InfernalExpansion.TAB)));
 }
