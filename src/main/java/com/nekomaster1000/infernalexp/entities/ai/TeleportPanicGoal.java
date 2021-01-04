f1d57cbd8b6f4e0f17f7d24a313cd8a26c367d39
HellionGames <dsgillespie02@yahoo.com>
2021-01-04 12:43:10 -0500

    Spider/Warpbeetle Crash Fix
    -Warpbeetle and Spider crash fix

--- src/main/java/com/nekomaster1000/infernalexp/entities/ai/TeleportPanicGoal.java
+++ src/main/java/com/nekomaster1000/infernalexp/entities/ai/TeleportPanicGoal.java
@@ -5,6 +5,8 @@ import net.minecraft.entity.ai.RandomPositionGenerator;
 import net.minecraft.entity.ai.goal.PanicGoal;
 import net.minecraft.util.math.vector.Vector3d;
 
+import java.util.List;
+
 public class TeleportPanicGoal extends PanicGoal {
 
     public TeleportPanicGoal(CreatureEntity creature, double speedIn) {
@@ -16,6 +18,7 @@ public class TeleportPanicGoal extends PanicGoal {
         this.creature.attemptTeleport(this.randPosX, this.randPosY, this.randPosZ, true); //Final parameter determines whether or not purple enderman particles appear on teleport
         this.creature.setMotion(0.0D, 0.0D, 0.0D);
         this.creature.setRevengeTarget(null);
+        removeTargeting();
     }
 
     @Override
@@ -34,6 +37,20 @@ public class TeleportPanicGoal extends PanicGoal {
             this.randPosZ = vector3d.z;
 
             return true;
+        }
+    }
+
+    private void removeTargeting(){
+        List<?> list = this.creature.world.getEntitiesWithinAABB(CreatureEntity.class,
+                this.creature.getBoundingBox().grow(32.0D));
+
+        for(int j = 0; j < list.size(); j++)
+        {
+            CreatureEntity entity1 = (CreatureEntity)list.get(j);
+            if(entity1.getAttackTarget() == this.creature)
+            {
+                entity1.setAttackTarget(null);
+            }
         }
     }
 }
