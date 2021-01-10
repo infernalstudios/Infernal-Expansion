6a56c1f47b14bfbf88cda2b94c64cbaa68eb0bda
Cael Warner <52902343+CaelWarner@users.noreply.github.com>
2021-01-09 19:12:08 -0800

    Luminous Fungus now spawn on the ceiling too
    - Added individual rates for luminous fungus spawning on the ceiling and on the floor

--- src/main/java/com/nekomaster1000/infernalexp/world/gen/features/LuminousFungusFeature.java
+++ src/main/java/com/nekomaster1000/infernalexp/world/gen/features/LuminousFungusFeature.java
@@ -1,7 +1,10 @@
 package com.nekomaster1000.infernalexp.world.gen.features;
 
 import com.mojang.serialization.Codec;
+import com.nekomaster1000.infernalexp.blocks.LuminousFungusBlock;
 import com.nekomaster1000.infernalexp.init.ModBlocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.state.properties.AttachFace;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.ISeedReader;
 import net.minecraft.world.gen.ChunkGenerator;
@@ -16,13 +19,35 @@ public class LuminousFungusFeature extends Feature<NoFeatureConfig> {
     }
 
     @Override
-    public boolean generate(ISeedReader worldIn, ChunkGenerator generator, Random random, BlockPos pos, NoFeatureConfig config) {
-        if (!worldIn.isAirBlock(pos) || worldIn.getBlockState(pos.down()).getBlock() != ModBlocks.GLOWDUST_SAND.get()) {
-            return false;
+    public boolean generate(ISeedReader world, ChunkGenerator generator, Random random, BlockPos pos, NoFeatureConfig config) {
+        int i = 0;
+        int amount;
+        AttachFace face;
+
+        int pickFacing = random.nextInt(3);
+
+        if (pickFacing == 0) {
+            face = AttachFace.CEILING;
+            amount = 4;
         } else {
-            worldIn.setBlockState(pos, ModBlocks.LUMINOUS_FUNGUS.get().getDefaultState(), 10);
-            return true;
+            face = AttachFace.FLOOR;
+            amount = 10;
         }
+
+        for (int j = 0; j < 96; j++) {
+            BlockState state = ModBlocks.LUMINOUS_FUNGUS.get().getDefaultState().with(LuminousFungusBlock.FACE, face);
+            BlockPos blockpos = pos.add(random.nextInt(20) - random.nextInt(40), random.nextInt(4) - random.nextInt(8), random.nextInt(20) - random.nextInt(40));
+
+            if (world.isAirBlock(blockpos) && state.isValidPosition(world, blockpos) && (world.getBlockState(blockpos.up()) == ModBlocks.DULLSTONE.get().getDefaultState() || world.getBlockState(blockpos.down()) == ModBlocks.GLOWDUST_SAND.get().getDefaultState())) {
+                world.setBlockState(blockpos, state, 10);
+                i++;
+            }
+
+            if (i >= amount)
+                return true;
+        }
+
+        return false;
     }
 }
 
