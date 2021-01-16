aaa75cdcd02f335df197bef7013a68c8e3994fe3
reetamb <57857847+reetamb@users.noreply.github.com>
2021-01-16 14:24:38 -0800

    reworked blackstone and crumbling blackstone and blackstone rubble to use ores rather than blobs (may need some rarity tweaking)

---
+++ src/main/java/com/nekomaster1000/infernalexp/init/ModTags.java
@@ -0,0 +1,20 @@
+package com.nekomaster1000.infernalexp.init;
+
+import com.nekomaster1000.infernalexp.InfernalExpansion;
+import net.minecraft.block.Block;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ITag;
+import net.minecraft.util.ResourceLocation;
+
+public class ModTags {
+
+    public static class Blocks {
+
+        public static final ITag.INamedTag<Block> BASE_STONE_CANYON = tag("base_stone_canyon");
+        public static final ITag.INamedTag<Block> BASE_STONE_SHORES = tag("base_stone_shores");
+
+        private static ITag.INamedTag<Block> tag(String name) {
+            return BlockTags.createOptional(new ResourceLocation(InfernalExpansion.MOD_ID, name));
+        }
+    }
+}
--- src/main/java/com/nekomaster1000/infernalexp/world/biome/netherbiomes/GlowstoneCanyonBiome.java
+++ src/main/java/com/nekomaster1000/infernalexp/world/biome/netherbiomes/GlowstoneCanyonBiome.java
@@ -70,10 +70,10 @@ public class GlowstoneCanyonBiome extends ModBiome {
         generation.withFeature(GenerationStage.Decoration.SURFACE_STRUCTURES, ModConfiguredFeatures.DULLSTONE_DEATH_PIT);
         generation.withFeature(GenerationStage.Decoration.VEGETAL_DECORATION, ModConfiguredFeatures.LUMINOUS_FUNGUS);
         generation.withFeature(GenerationStage.Decoration.VEGETAL_DECORATION, ModConfiguredFeatures.DULLTHORNS);
-        generation.withFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, ModConfiguredFeatures.GLOWCANYON_BLACKSTONE_BLOBS);
-        generation.withFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, ModConfiguredFeatures.GLOWCANYON_CRUMBLING_BLACKSTONE_BLOBS);
         generation.withFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Features.SPRING_LAVA);
         generation.withFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Features.SPRING_LAVA_DOUBLE);
+        generation.withFeature(GenerationStage.Decoration.UNDERGROUND_ORES, ModConfiguredFeatures.CANYON_BLACKSTONE_ORE);
+        generation.withFeature(GenerationStage.Decoration.UNDERGROUND_ORES, ModConfiguredFeatures.CANYON_CRUMBLING_BLACKSTONE_ORE);
         generation.withFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, Features.BLACKSTONE_BLOBS);
         generation.withFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, Features.PATCH_FIRE);
         generation.withFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, Features.PATCH_SOUL_FIRE);
