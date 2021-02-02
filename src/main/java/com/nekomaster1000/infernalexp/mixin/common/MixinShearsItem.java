7ea32dae941b4115027370bed02ffed6f3ee1c39
SwanX1 <karliscern@gmail.com>
2021-02-02 15:57:50 +0200

    dullthorn fixes

---
+++ src/main/java/com/nekomaster1000/infernalexp/mixin/common/MixinShearsItem.java
@@ -0,0 +1,21 @@
+package com.nekomaster1000.infernalexp.mixin.common;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.nekomaster1000.infernalexp.init.ModBlocks;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ShearsItem;
+
+@Mixin(ShearsItem.class)
+public class MixinShearsItem {
+    @Inject(method = "getDestroySpeed", at = @At("HEAD"), cancellable = true)
+    private void getDestroySpeed(ItemStack stack, BlockState state, CallbackInfoReturnable<Float> ci) {
+        if (state.isIn(ModBlocks.DULLTHORNS.get())) ci.setReturnValue(15.0F);
+        return;
+    }
+}
