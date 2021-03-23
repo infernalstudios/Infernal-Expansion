48f94b12b4a06eac8cac687d7b4ab28838cee3c3
LudoCrypt <60561627+LudoCrypt@users.noreply.github.com>
2021-03-22 22:33:37 -0500

    Update src/main/java/com/nekomaster1000/infernalexp/access/SoulFireAccess.java
    Co-authored-by: Cael Warner <52902343+CaelWarner@users.noreply.github.com>

--- src/main/java/com/nekomaster1000/infernalexp/access/SoulFireAccess.java
+++ src/main/java/com/nekomaster1000/infernalexp/access/SoulFireAccess.java
@@ -29,7 +29,10 @@ public interface SoulFireAccess {
 	public static final RenderMaterial LOCATION_ENDER_FIRE_1 = new RenderMaterial(AtlasTexture.LOCATION_BLOCKS_TEXTURE, new ResourceLocation("endergetic", "block/ender_fire_1"));
 
 	public static enum KnownFireTypes {
-		FIRE("fire", ModelBakery.LOCATION_FIRE_0, ModelBakery.LOCATION_FIRE_1), SOUL_FIRE("soul_fire", LOCATION_SOUL_FIRE_0, LOCATION_SOUL_FIRE_1), FIRE_GLOW("fire_glow", LOCATION_FIRE_0_GLOW, LOCATION_FIRE_1_GLOW), ENDER_FIRE("ender_fire", LOCATION_ENDER_FIRE_0, LOCATION_ENDER_FIRE_1);
+		FIRE("fire", ModelBakery.LOCATION_FIRE_0, ModelBakery.LOCATION_FIRE_1),
+		SOUL_FIRE("soul_fire", LOCATION_SOUL_FIRE_0, LOCATION_SOUL_FIRE_1),
+		GLOW_FIRE("glow_fire", LOCATION_GLOW_FIRE_0, LOCATION_GLOW_FIRE_1),
+		ENDER_FIRE("ender_fire", LOCATION_ENDER_FIRE_0, LOCATION_ENDER_FIRE_1);
 
 		public static final KnownFireTypes[] VALUES = values();
 		public static final Map<String, KnownFireTypes> NAME_LOOKUP = Arrays.stream(VALUES).collect(Collectors.toMap(KnownFireTypes::getName, (fireType) -> {
