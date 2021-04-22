b6a0f8a86fd93bf09cd0c85626179ff363bccbb0
Cael Warner <52902343+CaelWarner@users.noreply.github.com>
2021-04-22 12:21:04 -0700

    Fixed MixinPaintingEntity (hopefully)
    - I can't personally test if I've fixed the error because I couldn't recreate it in the first place
    - So hopefully this will fix it

--- src/main/java/com/nekomaster1000/infernalexp/mixin/common/MixinPaintingEntity.java
+++ src/main/java/com/nekomaster1000/infernalexp/mixin/common/MixinPaintingEntity.java
@@ -16,10 +16,10 @@ public class MixinPaintingEntity {
 	public PaintingType art;
 
 	// Minecraft always wants to place the biggest painting possible, this makes sure that it doesn't think that an Infernal Expansion painting is the biggest possible
-	@ModifyVariable(at = @At(value = "STORE"), method = "<init>(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/Direction;)V", name = "j", ordinal = 1)
-	public int IE_changeMaxSurfaceArea(int maxSurfaceArea) {
-		return art.getRegistryName().getNamespace().equals("infernalexp") ? 0 : maxSurfaceArea;
-	}
+    @ModifyVariable(at = @At(value = "STORE"), method = "<init>(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/Direction;)V", name = "j", ordinal = 1, index = 8)
+    public int IE_changeMaxSurfaceArea(int maxSurfaceArea) {
+        return art.getRegistryName().getNamespace().equals("infernalexp") ? 0 : maxSurfaceArea;
+    }
 
 	@ModifyVariable(at = @At(value = "INVOKE", target = "Ljava/util/List;isEmpty()Z"), method = "<init>(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/Direction;)V", name = "list")
 	public List<PaintingType> IE_changePaintingList(List<PaintingType> list) {
