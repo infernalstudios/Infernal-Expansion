976580663dea4e83520899d95d8fe33e44b1b103
LudoCrypt <60561627+LudoCrypt@users.noreply.github.com>
2021-04-22 19:49:52 -0500

    Update MixinPaintingEntity.java

--- src/main/java/com/nekomaster1000/infernalexp/mixin/common/MixinPaintingEntity.java
+++ src/main/java/com/nekomaster1000/infernalexp/mixin/common/MixinPaintingEntity.java
@@ -16,7 +16,7 @@ public class MixinPaintingEntity {
 	public PaintingType art;
 
 	// Minecraft always wants to place the biggest painting possible, this makes sure that it doesn't think that an Infernal Expansion painting is the biggest possible
-    @ModifyVariable(at = @At(value = "STORE"), method = "<init>(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/Direction;)V", name = "j", ordinal = 1, index = 8)
+    @ModifyVariable(at = @At(value = "STORE"), method = "<init>(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/Direction;)V", ordinal = 1)
     public int IE_changeMaxSurfaceArea(int maxSurfaceArea) {
         return art.getRegistryName().getNamespace().equals("infernalexp") ? 0 : maxSurfaceArea;
     }
