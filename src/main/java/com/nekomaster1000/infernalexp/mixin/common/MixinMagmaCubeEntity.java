c498a2dc6c94d6e0d210fcfd1677b0942e93665d
BlackGear27 <itsblackgear@gmail.com>
2021-04-23 21:35:09 -0400

    bucketable entities + voline attack
    + small magma cubes, volines and baby striders are now bucketable
    + voline now attack unnamed small magma cubes
    
    KNOWN ISSUES:
    - magma cubes don't drop magma cream once killed by volines
    - baby striders spawned from buckets can spawn with another baby strider on top or even a zombified piglin

---
+++ src/main/java/com/nekomaster1000/infernalexp/mixin/common/MixinMagmaCubeEntity.java
@@ -0,0 +1,93 @@
+package com.nekomaster1000.infernalexp.mixin.common;
+
+import com.nekomaster1000.infernalexp.init.IEItems;
+import com.nekomaster1000.infernalexp.util.IBucketable;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.monster.MagmaCubeEntity;
+import net.minecraft.entity.monster.SlimeEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+
+//<>
+
+@Mixin(MagmaCubeEntity.class)
+public abstract class MixinMagmaCubeEntity extends SlimeEntity implements IBucketable {
+    private static final DataParameter<Boolean> FROM_BUCKET = EntityDataManager.createKey(MagmaCubeEntity.class, DataSerializers.BOOLEAN);
+
+    public MixinMagmaCubeEntity(EntityType<? extends SlimeEntity> type, World worldIn) {
+        super(type, worldIn);
+    }
+
+    @Override
+    protected void registerData() {
+        super.registerData();
+        this.dataManager.register(FROM_BUCKET, false);
+    }
+
+    @Override
+    public boolean isFromBucket() {
+        return this.dataManager.get(FROM_BUCKET);
+    }
+
+    @Override
+    public void setFromBucket(boolean isFromBucket) {
+        this.dataManager.set(FROM_BUCKET, isFromBucket);
+    }
+
+    @Override
+    public void writeAdditional(CompoundNBT compound) {
+        super.writeAdditional(compound);
+        compound.putBoolean("FromBucket", this.isFromBucket());
+    }
+
+    @Override
+    public void readAdditional(CompoundNBT compound) {
+        super.readAdditional(compound);
+        this.setFromBucket(compound.getBoolean("FromBucket"));
+    }
+
+    @Override
+    protected ActionResultType getEntityInteractionResult(PlayerEntity playerIn, Hand hand) {
+        if (this.isSmallSlime()) {
+            return IBucketable.tryBucketEntity(playerIn, hand, this).orElse(super.getEntityInteractionResult(playerIn, hand));
+        } else {
+            return super.getEntityInteractionResult(playerIn, hand);
+        }
+    }
+
+    @Override
+    public void copyToStack(ItemStack stack) {
+        CompoundNBT compound = stack.getOrCreateTag();
+        IBucketable.copyToStack(this, stack);
+
+        compound.putInt("Size", this.getSlimeSize());
+    }
+
+    @Override
+    public void copyFromAdditional(CompoundNBT compound) {
+        IBucketable.copyFromAdditional(this, compound);
+        if (compound.contains("Size", 99)) {
+            this.setSlimeSize(compound.getInt("Size"), false);
+        }
+    }
+
+    @Override
+    public SoundEvent getBucketedSound() {
+        return SoundEvents.ITEM_BUCKET_FILL_LAVA;
+    }
+
+    @Override
+    public ItemStack getBucketItem() {
+        return new ItemStack(IEItems.MAGMA_CUBE_BUCKET.get());
+    }
+}
---
+++ src/main/java/com/nekomaster1000/infernalexp/util/IBucketable.java
@@ -0,0 +1,112 @@
+package com.nekomaster1000.infernalexp.util;
+
+import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.world.World;
+
+import java.util.Optional;
+
+//<>
+
+public interface IBucketable {
+    boolean isFromBucket();
+
+    void setFromBucket(boolean isFromBucket);
+
+    void copyToStack(ItemStack stack);
+
+    void copyFromAdditional(CompoundNBT compound);
+
+    ItemStack getBucketItem();
+
+    SoundEvent getBucketedSound();
+
+    static void copyToStack(MobEntity entity, ItemStack stack) {
+        CompoundNBT compound = stack.getOrCreateTag();
+        if (entity.hasCustomName()) {
+            stack.setDisplayName(entity.getCustomName());
+        }
+
+        if (entity.isAIDisabled()) {
+            compound.putBoolean("NoAI", entity.isAIDisabled());
+        }
+
+        if (entity.isSilent()) {
+            compound.putBoolean("Silent", entity.isSilent());
+        }
+
+        if (entity.hasNoGravity()) {
+            compound.putBoolean("NoGravity", entity.hasNoGravity());
+        }
+
+        if (entity.isGlowing()) {
+            compound.putBoolean("Glowing", entity.isGlowing());
+        }
+
+        if (entity.isInvulnerable()) {
+            compound.putBoolean("Invulnerable", entity.isInvulnerable());
+        }
+
+        compound.putFloat("Health", entity.getHealth());
+    }
+
+    static void copyFromAdditional(MobEntity entity, CompoundNBT compound) {
+        if (compound.contains("NoAI")) {
+            entity.setNoAI(compound.getBoolean("NoAI"));
+        }
+
+        if (compound.contains("Silent")) {
+            entity.setSilent(compound.getBoolean("Silent"));
+        }
+
+        if (compound.contains("NoGravity")) {
+            entity.setNoGravity(compound.getBoolean("NoGravity"));
+        }
+
+        if (compound.contains("Glowing")) {
+            entity.setGlowing(compound.getBoolean("Glowing"));
+        }
+
+        if (compound.contains("Invulnerable")) {
+            entity.setInvulnerable(compound.getBoolean("Invulnerable"));
+        }
+
+        if (compound.contains("Health", 99)) {
+            entity.setHealth(compound.getFloat("Health"));
+        }
+    }
+
+    static <T extends LivingEntity & IBucketable> Optional<ActionResultType> tryBucketEntity(PlayerEntity player, Hand hand, T entity) {
+        ItemStack heldItem = player.getHeldItem(hand);
+        if (heldItem.getItem() == Items.LAVA_BUCKET && entity.isAlive()) {
+            entity.playSound(entity.getBucketedSound(), 1.0F, 1.0F);
+            heldItem.shrink(1);
+            ItemStack bucketItem = entity.getBucketItem();
+            entity.copyToStack(bucketItem);
+            World world = entity.world;
+            if (!world.isRemote) {
+                CriteriaTriggers.FILLED_BUCKET.trigger((ServerPlayerEntity)player, bucketItem);
+            }
+
+            if (heldItem.isEmpty()) {
+                player.setHeldItem(hand, bucketItem);
+            } else if (!player.inventory.addItemStackToInventory(bucketItem)) {
+                player.dropItem(bucketItem, false);
+            }
+
+            entity.remove();
+            return Optional.of(ActionResultType.func_233537_a_(world.isRemote));
+        } else {
+            return Optional.empty();
+        }
+    }
+}
