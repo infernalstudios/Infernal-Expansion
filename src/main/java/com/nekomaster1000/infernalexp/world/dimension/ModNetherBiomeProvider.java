1b12cddad224cd670736383ae80552c3a0ffa2d2
Dariensg <57117148+Dariensg@users.noreply.github.com>
2021-04-29 13:44:55 -0400

    Update src/main/java/com/nekomaster1000/infernalexp/world/dimension/ModNetherBiomeProvider.java
    Co-authored-by: SwanX1 <karliscern@gmail.com>

--- src/main/java/com/nekomaster1000/infernalexp/world/dimension/ModNetherBiomeProvider.java
+++ src/main/java/com/nekomaster1000/infernalexp/world/dimension/ModNetherBiomeProvider.java
@@ -91,8 +91,7 @@ public class ModNetherBiomeProvider extends BiomeProvider {
             } else {
                 return this.biomeRegistry.getValueForKey(BiomeRegistry.getKeyFromID(0));
             }
-        }
-        else {
+        } else {
             return biome;
         }
     }
