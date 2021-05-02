3aa1c60dcc57962490267fb0bc30d374d215ea8c
HellionGames <dsgillespie02@yahoo.com>
2021-05-02 09:10:36 -0400

    Strider Altars in Lava Oceans
    -Strider Altars now generate in Lava Oceans only
    -Increased Separation and Spacing of Strider Altars to make them less common

--- src/main/java/com/nekomaster1000/infernalexp/world/gen/structures/StriderAltarStructure.java
+++ src/main/java/com/nekomaster1000/infernalexp/world/gen/structures/StriderAltarStructure.java
@@ -2,11 +2,15 @@ package com.nekomaster1000.infernalexp.world.gen.structures;
 
 import com.mojang.serialization.Codec;
 import com.nekomaster1000.infernalexp.InfernalExpansion;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.util.Direction;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MutableBoundingBox;
 import net.minecraft.util.registry.DynamicRegistries;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.IBlockReader;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.ChunkGenerator;
 import net.minecraft.world.gen.GenerationStage;
@@ -35,13 +39,8 @@ public class StriderAltarStructure extends IEStructure<NoFeatureConfig> {
 
     @Override
     public StructureSeparationSettings getSeparationSettings() {
-        return new StructureSeparationSettings(4, 2, 80837592);
+        return new StructureSeparationSettings(10, 5, 80837592);
     }
-
-//	@Override
-//	protected boolean func_230363_a_(ChunkGenerator chunkGenerator, BiomeProvider biomeProvider, long seed, SharedSeedRandom chunkRandom, int chunkX, int chunkZ, Biome biome, ChunkPos chunkPos, NoFeatureConfig config) {
-//		return super.func_230363_a_(chunkGenerator, biomeProvider, seed, chunkRandom, chunkX, chunkZ, biome, chunkPos, config);
-//	}
 
     @Override
     public boolean shouldTransformLand() {
@@ -53,6 +52,32 @@ public class StriderAltarStructure extends IEStructure<NoFeatureConfig> {
             super(structure, chunkX, chunkZ, mutableBoundingBox, reference, seed);
         }
 
+        public int getLavaY(ChunkGenerator chunkGenerator, int x, int z) {
+            int y = chunkGenerator.getSeaLevel();
+            IBlockReader blockColumn = chunkGenerator.func_230348_a_(x, z);
+
+            BlockPos pos = new BlockPos(x, y, z);
+
+            int topDown = 1;
+
+            if (blockColumn.getBlockState(pos).isSolid()) {
+                return 0;
+            }
+
+            while (topDown <= y) {
+                BlockState checkLava = blockColumn.getBlockState(pos.down(topDown));
+                BlockState checkBlock = blockColumn.getBlockState(pos.down(topDown + 1));
+
+                if (checkLava.matchesBlock(Blocks.LAVA) && checkBlock.isSolidSide(blockColumn, pos.down(y), Direction.UP)) {
+                    return y-topDown;
+                }
+
+                topDown++;
+            }
+
+            return 0;
+        }
+
 
 
         @Override
@@ -60,7 +85,7 @@ public class StriderAltarStructure extends IEStructure<NoFeatureConfig> {
             int x = (chunkX << 4) + this.rand.nextInt(16);
             int z = (chunkZ << 4) + this.rand.nextInt(16);
 
-            BlockPos pos = new BlockPos(x, getYPos(chunkGenerator, x, z), z);
+            BlockPos pos = new BlockPos(x, getLavaY(chunkGenerator, x, z), z);
 
             if (pos.getY() != 0) {
                 JigsawManager.func_242837_a(
