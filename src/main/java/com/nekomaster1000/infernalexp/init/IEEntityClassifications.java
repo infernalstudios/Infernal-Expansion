9c6b3d8a3128537a1c79f3c02779a1b20bf91a80
HellionGames <dsgillespie02@yahoo.com>
2021-05-07 17:54:30 -0400

    Dumb Mistake
    -Whoops

--- src/main/java/com/nekomaster1000/infernalexp/init/IEEntityClassifications.java
+++ src/main/java/com/nekomaster1000/infernalexp/init/IEEntityClassifications.java
@@ -8,6 +8,6 @@ public class IEEntityClassifications {
 
     public static void register() {
         IEMOB = EntityClassification.create("IE_mob", "IEMOB", 70, true, false, 128);
-        InfernalExpansion.LOGGER.info("Infernal Expansion: Items Registered!");
+        InfernalExpansion.LOGGER.info("Infernal Expansion: Entity Classifications Registered!");
     }
 }
