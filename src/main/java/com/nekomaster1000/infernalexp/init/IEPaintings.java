07afd885016f59298e11a01858a68ded491ed0c9
Neko <nekozzz@live.co.uk>
2021-06-04 23:40:57 +0100

    New Paintings by Laz!

--- src/main/java/com/nekomaster1000/infernalexp/init/IEPaintings.java
+++ src/main/java/com/nekomaster1000/infernalexp/init/IEPaintings.java
@@ -14,6 +14,14 @@ public class IEPaintings {
 	public static RegistryObject<PaintingType> CHILLING_ISLES = PAINTING_TYPES.register("chilling_isles", () -> new PaintingType(48, 48));
 	public static RegistryObject<PaintingType> VOLINE = PAINTING_TYPES.register("voline", () -> new PaintingType(16, 16));
 	public static RegistryObject<PaintingType> DA_SALT = PAINTING_TYPES.register("da_salt", () -> new PaintingType(64, 32));
+    public static RegistryObject<PaintingType> GLOWSTONE_CANYON_PAINTING = PAINTING_TYPES.register("glowstone_canyon_painting", () -> new PaintingType(64, 64));
+    public static RegistryObject<PaintingType> SOUL_BY_SOUL = PAINTING_TYPES.register("soul_by_soul", () -> new PaintingType(80, 48));
+    public static RegistryObject<PaintingType> GARRY = PAINTING_TYPES.register("pineapple_under_the_lava_sea", () -> new PaintingType(64, 64));
+    public static RegistryObject<PaintingType> SHROOM_DUALITY = PAINTING_TYPES.register("shroom_duality", () -> new PaintingType(16, 16));
+    public static RegistryObject<PaintingType> PIGS_GREED = PAINTING_TYPES.register("pigs_greed", () -> new PaintingType(16, 16));
+    public static RegistryObject<PaintingType> SOUL_HOLE = PAINTING_TYPES.register("soul_hole", () -> new PaintingType(16, 16));
+    public static RegistryObject<PaintingType> GLOW_LIKE_HELLY = PAINTING_TYPES.register("glow_like_helly", () -> new PaintingType(16, 16));
+
 
 	public static void register(IEventBus eventBus) {
 		PAINTING_TYPES.register(eventBus);
