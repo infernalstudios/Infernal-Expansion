ae60aa68a775243eb7d773f9935e9e23a44231ab
HellionGames <dsgillespie02@yahoo.com>
2021-07-09 21:50:30 -0400

    Added Automatic Installation of Infernal Resources
    -Added to ClientSetup to copy Infernal Resources to MC Resource Pack folder
    -Fixed the directory of the textures in the resource pack zip

--- src/main/java/com/nekomaster1000/infernalexp/InfernalExpansion.java
+++ src/main/java/com/nekomaster1000/infernalexp/InfernalExpansion.java
@@ -61,6 +61,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -208,6 +212,32 @@ public class InfernalExpansion {
 
     private void clientSetup(final FMLClientSetupEvent event) {
         DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> InfernalExpansionClient::init);
+
+        loadInfernalResources();
+    }
+
+    private static void loadInfernalResources() {
+        // Creates file location for resource pack
+        File dir = new File(".", "resourcepacks");
+        File target = new File(dir, "Infernal Resources.zip");
+
+        // If the pack isn't already in the folder, copies the file over from the mod files
+        if(!target.exists())
+            try {
+                dir.mkdirs();
+                InputStream in = InfernalExpansion.class.getResourceAsStream("/assets/infernalexp/infernal_resources.zip");
+                FileOutputStream out = new FileOutputStream(target);
+
+                byte[] buf = new byte[16384];
+                int len = 0;
+                while((len = in.read(buf)) > 0)
+                    out.write(buf, 0, len);
+
+                in.close();
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
     }
 
     @SubscribeEvent
