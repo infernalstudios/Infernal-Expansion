cb4cd50a4386b183e3d124a7e5378ecdb08ba4e2
HellionGames <dsgillespie02@yahoo.com>
2021-07-18 10:01:56 -0400

    Requested Fixes
    -Hopefully the indentation is consistent because it doesn't have a visual difference in my IDE
    -Fixed alphabetical ordering of mixins

--- src/main/java/com/nekomaster1000/infernalexp/mixin/client/MixinItemModelsProperties.java
+++ src/main/java/com/nekomaster1000/infernalexp/mixin/client/MixinItemModelsProperties.java
@@ -20,19 +20,19 @@ public class MixinItemModelsProperties {
 
 	@ModifyVariable(method = "call", at = @At(value = "STORE", ordinal = 1 /* this ordinal is when its set to Math.random(), the second time d0 is set to something */), ordinal = 0 /* this ordinal means the first double variable */)
 	private double IE_daytimeInGSC(double in, ItemStack stack, @Nullable ClientWorld world, @Nullable LivingEntity entity) {
-        ClientWorld clientWorld = world;
+	    ClientWorld clientWorld = world;
         if (entity == null) {
             return in;
         }
         if (world == null && entity.world instanceof ClientWorld) {
-	        clientWorld = (ClientWorld)entity.getEntityWorld();
+            clientWorld = (ClientWorld)entity.getEntityWorld();
         }
 
         Optional<RegistryKey<Biome>> biome = clientWorld.func_242406_i(entity.getPosition());
 
-	    if (biome.isPresent() && biome.get().getLocation().equals(IEBiomes.GLOWSTONE_CANYON.getId())) {
-	        return MathHelper.nextDouble(new Random(), 0.95, 1.05) % 1;
+        if (biome.isPresent() && biome.get().getLocation().equals(IEBiomes.GLOWSTONE_CANYON.getId())) {
+            return MathHelper.nextDouble(new Random(), 0.95, 1.05) % 1;
         }
-		return in;
+        return in;
 	}
 }
