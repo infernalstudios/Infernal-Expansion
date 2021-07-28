c8a0b30b092d57297f010face439c97e157065bc
HellionGames <dsgillespie02@yahoo.com>
2021-07-28 08:20:58 -0400

    Revert "Fix Glowstone duplication issue"
    This reverts commit a7b9dd21312ce5f97a49ca923893c8a5471270a0.

--- src/main/java/com/nekomaster1000/infernalexp/events/MiscEvents.java
+++ src/main/java/com/nekomaster1000/infernalexp/events/MiscEvents.java
@@ -162,7 +162,6 @@ public class MiscEvents {
         if (heldItemStack.getItem() == Items.GLOWSTONE_DUST) {
             if (world.getBlockState(pos).getBlock() == IEBlocks.DIMSTONE.get()) {
                 player.swingArm(event.getHand());
-                heldItemStack.shrink(2);
                 for (int i = 0; i < 20; i++) {
                     world.addParticle(IEParticleTypes.GLOWSTONE_SPARKLE.get(), pos.getX(), pos.getY(), pos.getZ(), 0.0, 0.0, 0.0);
                 }
@@ -170,7 +169,6 @@ public class MiscEvents {
                 world.setBlockState(pos, Blocks.GLOWSTONE.getDefaultState());
             } else if (world.getBlockState(pos).getBlock() == IEBlocks.DULLSTONE.get()) {
                 player.swingArm(event.getHand());
-                heldItemStack.shrink(2);
                 for (int i = 0; i < 20; i++) {
                     world.addParticle(IEParticleTypes.GLOWSTONE_SPARKLE.get(), pos.getX(), pos.getY(), pos.getZ(), 0.0, 0.0, 0.0);
                 }
