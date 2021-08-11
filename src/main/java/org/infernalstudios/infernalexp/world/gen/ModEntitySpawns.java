a494528bf6c832e900f2cec749d12c93a2f55a42
Cael Warner <52902343+CaelWarner@users.noreply.github.com>
2021-08-11 13:48:12 -0700

    Updated GlobalEntityTypeAttributes to use EntityAttributeCreationEvent

--- src/main/java/org/infernalstudios/infernalexp/world/gen/ModEntitySpawns.java
+++ src/main/java/org/infernalstudios/infernalexp/world/gen/ModEntitySpawns.java
@@ -16,6 +16,9 @@
 
 package org.infernalstudios.infernalexp.world.gen;
 
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import org.infernalstudios.infernalexp.InfernalExpansion;
 import org.infernalstudios.infernalexp.entities.BasaltGiantEntity;
 import org.infernalstudios.infernalexp.entities.BlackstoneDwarfEntity;
@@ -28,29 +31,23 @@ import org.infernalstudios.infernalexp.entities.VolineEntity;
 import org.infernalstudios.infernalexp.entities.WarpbeetleEntity;
 import org.infernalstudios.infernalexp.init.IEEntityTypes;
 
-import net.minecraft.entity.ai.attributes.GlobalEntityTypeAttributes;
-
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
-
 //import net.minecraft.entity.monster.MonsterEntity;
 
 @Mod.EventBusSubscriber(modid = InfernalExpansion.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModEntitySpawns {
 
     @SubscribeEvent
-    public static void spawnEntities(FMLLoadCompleteEvent event) {
-        GlobalEntityTypeAttributes.put(IEEntityTypes.VOLINE.get(), VolineEntity.setCustomAttributes().create());
-        GlobalEntityTypeAttributes.put(IEEntityTypes.SHROOMLOIN.get(), ShroomloinEntity.setCustomAttributes().create());
-        GlobalEntityTypeAttributes.put(IEEntityTypes.WARPBEETLE.get(), WarpbeetleEntity.setCustomAttributes().create());
-        //GlobalEntityTypeAttributes.put(IEEntityTypes.CEROBEETLE.get(), CerobeetleEntity.setCustomAttributes().create());
-        GlobalEntityTypeAttributes.put(IEEntityTypes.EMBODY.get(), EmbodyEntity.setCustomAttributes().create());
-        GlobalEntityTypeAttributes.put(IEEntityTypes.BASALT_GIANT.get(), BasaltGiantEntity.setCustomAttributes().create());
-        GlobalEntityTypeAttributes.put(IEEntityTypes.BLACKSTONE_DWARF.get(), BlackstoneDwarfEntity.setCustomAttributes().create());
-        GlobalEntityTypeAttributes.put(IEEntityTypes.GLOWSQUITO.get(), GlowsquitoEntity.setCustomAttributes().create());
-        //GlobalEntityTypeAttributes.put(IEEntityTypes.PYRNO.get(), PyrnoEntity.setCustomAttributes().create());
-        GlobalEntityTypeAttributes.put(IEEntityTypes.BLINDSIGHT.get(), BlindsightEntity.setCustomAttributes().create());
-        GlobalEntityTypeAttributes.put(IEEntityTypes.GLOWSILK_MOTH.get(), GlowsilkMothEntity.setCustomAttributes().create());
+    public static void addEntityAttributes(EntityAttributeCreationEvent event) {
+        event.put(IEEntityTypes.VOLINE.get(), VolineEntity.setCustomAttributes().create());
+        event.put(IEEntityTypes.SHROOMLOIN.get(), ShroomloinEntity.setCustomAttributes().create());
+        event.put(IEEntityTypes.WARPBEETLE.get(), WarpbeetleEntity.setCustomAttributes().create());
+        //event.put(IEEntityTypes.CEROBEETLE.get(), CerobeetleEntity.setCustomAttributes().create());
+        event.put(IEEntityTypes.EMBODY.get(), EmbodyEntity.setCustomAttributes().create());
+        event.put(IEEntityTypes.BASALT_GIANT.get(), BasaltGiantEntity.setCustomAttributes().create());
+        event.put(IEEntityTypes.BLACKSTONE_DWARF.get(), BlackstoneDwarfEntity.setCustomAttributes().create());
+        event.put(IEEntityTypes.GLOWSQUITO.get(), GlowsquitoEntity.setCustomAttributes().create());
+        //event.put(IEEntityTypes.PYRNO.get(), PyrnoEntity.setCustomAttributes().create());
+        event.put(IEEntityTypes.BLINDSIGHT.get(), BlindsightEntity.setCustomAttributes().create());
+        event.put(IEEntityTypes.GLOWSILK_MOTH.get(), GlowsilkMothEntity.setCustomAttributes().create());
     }
 }
