4ff94377f67636c4469c25ba5cb0d1fa6fc3e2ad
Neko <47757977+nekomaster1000@users.noreply.github.com>
2022-04-08 03:57:29 +0100

    Update src/main/java/org/infernalstudios/infernalexp/init/IELootModifiers.java
    From Swan.
    
    Co-authored-by: Kārlis Čerņavskis <karliscern@gmail.com>

--- src/main/java/org/infernalstudios/infernalexp/init/IELootModifiers.java
+++ src/main/java/org/infernalstudios/infernalexp/init/IELootModifiers.java
@@ -54,12 +54,7 @@ public class IELootModifiers {
         @Nonnull
         @Override
         protected List<ItemStack> doApply(List<ItemStack> generatedLoot, LootContext context) {
-
-            if ((context.getRandom().nextDouble() < 0.6)) {
-                return generatedLoot;
-            }
-
-            if (!InfernalExpansionConfig.MobInteractions.USE_HOGCHOPS.getBoolean()) {
+            if (context.getRandom().nextDouble() < 0.6 || !InfernalExpansionConfig.MobInteractions.USE_HOGCHOPS.getBoolean()) {
                 return generatedLoot;
             }
 
