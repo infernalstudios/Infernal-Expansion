ad0f5e80b342be0c610cf1f000841b4403768289
Cael Warner <52902343+CaelWarner@users.noreply.github.com>
2022-04-09 12:42:59 -0700

    Started adding Surface Rules
    - Added a way to append surface rules to the nether surface rule data
    - Current obstacle is that using this method surface rules are loaded really early in the game lifecycle, before Forge has loaded. This means that whenever I try to access any of our blocks the game crashes as they haven't been registered yet.

---
+++ src/main/java/org/infernalstudios/infernalexp/mixin/common/MixinSurfaceRuleData.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2021 Infernal Studios
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.infernalstudios.infernalexp.mixin.common;
+
+import net.minecraft.data.worldgen.SurfaceRuleData;
+import net.minecraft.world.level.levelgen.SurfaceRules;
+import org.infernalstudios.infernalexp.world.gen.surfacerules.IESurfaceRules;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Mixin(SurfaceRuleData.class)
+public class MixinSurfaceRuleData {
+
+    @Inject(method = "nether", at = @At("RETURN"), cancellable = true)
+    private static void IE_addNetherSurfaceRules(CallbackInfoReturnable<SurfaceRules.RuleSource> cir) {
+        if (cir.getReturnValue() instanceof SurfaceRules.SequenceRuleSource sequenceRuleSource) {
+            List<SurfaceRules.RuleSource> rules = new ArrayList<>(sequenceRuleSource.sequence());
+
+            rules.add(rules.size() - 1, IESurfaceRules.addNetherSurfaceRules());
+
+            cir.setReturnValue(new SurfaceRules.SequenceRuleSource(rules));
+        }
+    }
+
+}
