6de7df86e62c644ed86ee670055be3bde32ff68f
caelwarner <caelawarner@gmail.com>
2023-02-16 14:46:11 -0800

    Updated Quark glow shroom shroomloin type to new item id
    - Quark had changed the item id for the glow shroom from "glowshroom" to "glow_shroom". This meant that the glow shroom shroomloin type wouldn't apply
    - Fixes #392, fixes #382

--- src/main/java/org/infernalstudios/infernalexp/init/IEShroomloinTypes.java
+++ src/main/java/org/infernalstudios/infernalexp/init/IEShroomloinTypes.java
@@ -29,9 +29,9 @@ public final class IEShroomloinTypes {
     public static final ShroomloinType FAIRY_RING =               registerWithIEDirectory("fairy_ring_mushroom", "habitat",       "fairy_ring_mushroom");
     public static final ShroomloinType FUNGAL_IMPARIUS =          registerWithIEDirectory("fungal_imparius",     "byg",           "fungal_imparius");
     public static final ShroomloinType GLOWSHROOM_BOP =           registerWithIEDirectory("glowshroom",          "biomesoplenty", "glowshroom");
-    public static final ShroomloinType GLOWSHROOM_DARKER_DEPTHS = registerWithIEDirectory("glowshroom",          "darkerdepths",  "glowshroom");
-    public static final ShroomloinType GLOWSHROOM_QUARK =         registerWithIEDirectory("glowshroom",          "quark",         "glowshroom");
-    public static final ShroomloinType GREEN_MUSHROOM =           registerWithIEDirectory("green_mushroom",      "byg",           "green_mushroom");
+    public static final ShroomloinType GLOWSHROOM_DARKER_DEPTHS = registerWithIEDirectory("glowshroom", "darkerdepths", "glowshroom");
+    public static final ShroomloinType GLOWSHROOM_QUARK = registerWithIEDirectory("glow_shroom", "quark", "glowshroom");
+    public static final ShroomloinType GREEN_MUSHROOM = registerWithIEDirectory("green_mushroom", "byg", "green_mushroom");
     public static final ShroomloinType IMPARIUS_MUSHROOM =        registerWithIEDirectory("imparius_mushroom",   "byg",           "imparius_mushroom");
     public static final ShroomloinType INDIGO =                   registerWithIEDirectory("indigo_mushroom",     "undergarden",   "indigo_mushroom");
     public static final ShroomloinType INK =                      registerWithIEDirectory("ink_mushroom",        "undergarden",   "ink_mushroom");
